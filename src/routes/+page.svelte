4dbfc4bd92aa5cdf94c7faf110bef460bfc40bc0
Edgar Hildebrandt Rojo <edgarhr.usa@gmail.com>
2023-03-19 12:03:32 -0700

    Updated Data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,8 +29,8 @@
   const end_ago = new Date();
   end_ago.setDate(end_ago.getDate() - 4);
   
-  let start_date = "2023-03-09 23:59";
-  let end_date = "2023-03-10 23:59";
+  let start_date = "2023-03-16 23:59";
+  let end_date = "2023-03-17 23:59";
 
   // Sliders
   let minprice, maxprice, minvol, maxvol;
@@ -174,7 +174,7 @@
             inputClasses="w-1/2 ds-form-control ds-input ds-input-bordered"
             theme="my-theme"
             startDate="2022-04-01 00:00"
-            endDate="2023-03-10 23:59"
+            endDate="2023-03-17 23:59"
             todayBtn={false}
             clearBtn={false}
             format="yyyy-mm-dd hh:ii"
